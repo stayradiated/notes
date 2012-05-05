f5778cbcce5e36a186c6fb92714d1168b5d7abde
George Czabania <dev@stayradiated.com>
2012-05-05 18:18:47 +1200

    Replace font family with style set

--- js/notes.js
+++ js/notes.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-	$('.family').selectbox();
+	$('.style-set').selectbox();
 	
 	$('#style .bold').mousedown(function () {formatText('bold');return false;});
 	$('#style .italic').mousedown(function () {formatText('italic');return false;});
@@ -10,26 +10,9 @@ $(document).ready(function () {
 	$('#align .center').mousedown(function () {formatText('justifyCenter');	return false;});
 	$('#align .right').mousedown(function () {formatText('justifyRight');return false;});
 
-	$('#settings .family').change(function () {formatText('fontName', this.value)})
+	$('#settings .style-set').change(function () {formatText('formatBlock', this.value)})
 
 });
-
-function getSelText () {
-	var txt = '';
-
-	if (window.getSelection) {
-		txt = window.getSelection();
-
-	} else if (document.getSelection) {
-		txt = document.getSelection();
-
-	} else if (document.selection) {
-		txt = document.selection.createRange().text;
-
-	} else return;
-
-	return txt;
-}
 
 function formatText(command, option) {
 	document.execCommand(command, false, option);
